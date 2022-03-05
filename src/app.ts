ef6ad86d5c12c1bc15aa0b9f15c46f423c8c50c7
ibenchi <horizen27@gmail.com>
2022-03-05 15:48:41 +0000

    eventListner

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
   element: HTMLFormElement
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLTextAreaElement
+  peopleInputElement: HTMLInputElement
   constructor() {
     this.templateElement = document.getElementById(
       "project-input"
@@ -10,7 +13,26 @@ class ProjectInput {
     this.hostElement = document.getElementById("app")! as HTMLDivElement
     const importedNode = document.importNode(this.templateElement.content, true)
     this.element = importedNode.firstElementChild as HTMLFormElement
+    this.element.id = "user-input"
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLTextAreaElement
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement
+    this.configure()
     this.attach()
+  }
+  private submitHandler(event: Event) {
+    event.preventDefault()
+    alert("You just submited")
+  }
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler)
   }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element)
