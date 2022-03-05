ef030cc56c4c2400b62b90582081f736515476ab
ibenchi <horizen27@gmail.com>
2022-03-05 21:37:15 +0000

    More dom manipulation

--- src/app.ts
+++ src/app.ts
@@ -55,6 +55,35 @@ function AutoBind(_: any, __: string, descriptor: PropertyDescriptor) {
   }
   return adjDescriptor
 }
+// ProjectList class
+class ProjectList {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLElement
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    ) as HTMLTemplateElement
+    this.hostElement = document.getElementById("app") as HTMLDivElement
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLElement
+    this.element.id = `${this.type}-projects`
+    this.attach()
+    this.renderContent()
+  }
+  private renderContent() {
+    const listId = `${this.type}-projects-list`
+    this.element.querySelector("ul")!.id = listId
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS"
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element)
+  }
+}
+const ActiveProjectList = new ProjectList("active")
+const finishedProjectList = new ProjectList("finished")
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
